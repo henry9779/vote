6f02c06d3b740f5adb0db92f29d4eb63b27460bf
Henry <vinsen1109@gmail.com>
2020-11-10 22:37:14 +0800

    add delete action

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -24,8 +24,27 @@ class CandidatesController < ApplicationController
   end
 
   def edit
-    
+    @candidate = Candidate.find_by(id: params[:id])
   end
+
+  def update
+    @candidate = Candidate.find_by(id: params[:id])
+
+    if @candidate.update(candidate_params)
+      flash[:notice] = "Candidate Updated !"  
+      redirect_to '/candidates'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @candidate = Candidate.find_by(id: params[:id])
+    @candidate.destroy
+    flash[:notice] = "Candidate Deleted !"
+    redirect_to '/candidates'
+  end
+
 
   private
   def candidate_params
